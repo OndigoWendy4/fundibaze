34548bb0e2238f278971bf1ea0ee8183e2b0f596
OndigoWendy4 <ondigowendy.tech@gmail.com>
2021-08-19 16:37:57 +0300

    Fundi baze recreation using angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterBarComponent } from './footer-bar.componen';
+import { NavBarComponent } from './about-company/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterBarComponent
+    
   ],
   imports: [
     BrowserModule,
